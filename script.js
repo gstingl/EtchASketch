8182d87845cc2c6484c339dcb4b679d072d68214
Garret Stingl <garretstingl21@gmail.com>
2024-07-18 21:21:48 -0500

    Add two button, create function for clear and size change of grid, style page

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ const container = document.querySelector('.container');
 container.style.width = `${GRIDSIDE}px`;
 container.style.height = `${GRIDSIDE}px`;
 
+const button = document.querySelector('.gridSizeButton');
+button.addEventListener('click', function changeGridSize() {
+    deleteGrid();
+    rows = prompt('Enter a number between 1-50');
+    cols = rows;
+    if (rows <= 50) {
+    createGrid();
+    } else {
+        alert("Number is to large, please try again");
+        changeGridSize();
+    }
+});
+
+function deleteGrid() {
+    let del = document.querySelector('.container');
+    del.innerHTML = '';
+}
+
+const clearBtn = document.querySelector('.clearGrid');
+clearBtn.addEventListener('click', function clearGrid() {
+    deleteGrid();
+    createGrid();
+});
+
 function changeBackgroundColor () {
     this.style.backgroundColor = "black";
 }
